117fb4004ece6e7d6675e8a92febba9351c15774
Sony Gómez <sonygomez6@gmail.com>
2024-04-29 14:21:41 -0400

    Operación arimética en RSTUDIO

--- Prácticas/Rstudio#1ExerciseBasic.r
+++ Prácticas/Rstudio#1ExerciseBasic.r
@@ -2,5 +2,12 @@
 nombre <- readline(prompt =paste("Ingrese su primer nombre: "))
 apellido <- readline(prompt = paste("Ingrese su primer apellido: "))
 
-# Mostramos el saludo
-print(paste("Hola", nombre, apellido,"¡Un gusto saludarle!"))
+# Entrada de números
+num1 <- as.integer(readline(prompt=paste("¡Efectuemos una multiplicación! Ingresa un número: ")))
+num2 <- as.integer(readline(prompt=paste("Ingresa otro número: ")))
+
+# Almacenamos resultado en una variable
+resultado <- num1 * num2
+
+# Mostramos el saludo y su operación arimética
+print(paste("Hola", nombre, apellido,"¡Un gusto saludarle! El resultado de su multiplicación es:", resultado))
