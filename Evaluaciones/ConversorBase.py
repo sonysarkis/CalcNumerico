d00b521efc77c6e1d5a1eb6b73aeb0d9fd7b02b9
Sony Gómez <sonygomez6@gmail.com>
2024-05-12 11:11:14 -0400

    Conversor de bases. Decimal a todas. Backend

--- Evaluaciones/ConversorBase.py
+++ Evaluaciones/ConversorBase.py
@@ -11,6 +11,8 @@ def todo_a_decimal(num,c):
 
 
 # Método para convertir un número decimal a cualquier número con base c
+# Esto toma cualquier número decimal y dependiendo del valor que le demos a C, nos trasformará 
+# a esa base. Si c=8, nos dara un octal, si c=3, nos dara un terciario, y asi sucesivamente
 def decimal_a_cualquiera(n,c):
     # n será el número decimal que queremos convertir a cualquier base c
     if n < 2: # Si el número es menor a 2, retornamos el número en string porque ya no se puede dividir más 
@@ -18,25 +20,19 @@ def decimal_a_cualquiera(n,c):
     return decimal_a_cualquiera(n // c,c) + str(n % c) # Este método se llama recursivamente hasta que n sea menor a 2
                                                        # Se divide n entre c y se obtiene el cociente y el residuo de la división
                                                        # Se concatena el residuo a la cadena que se va formando
-                                                       # 
+                                                       
 
 op = int(input("""
             Presione [1] para ingresar un número binario.
+            Presione [2] para ingresar un número decimal.
             Ingrese su opción numérica:   """))
 
 if op == 1:     
     # Método para convertir de binario a decimal
-    def todo_a_decimal(num,c=2): # Inicializamos c en 2 para convertir binario a decimal, c siendo el divisor
+    def todo_a_decimal(num,c=2): # Inicializamos c=2 para que el divisor sea 2 y así convertir de binario a decimal
         if len(num) == 1:
             return int(num)
         return c * todo_a_decimal(num[:-1]) + int(num[-1])
-    
-     # Esto toma cualquier numero decimal y dependiendo del valor que le demos a C, nos trasformará 
-     # A esa base. Si c=8, nos dara un octal, si c=3, nos dara un terciario, y asi sucesivamente
-    def decimal_a_cualquiera(n,c):
-        if n < 2:
-            return str(n)
-        return decimal_a_cualquiera(n // c,c) + str(n % c)
      
     numero_binario = input("Ingrese un número binario: ")
     # Inicializamos c=3 para que 3 sea el divisor y poder obtener el número terciario
@@ -49,3 +45,22 @@ if op == 1:
     print(f"{numero_binario} en octal es: {decimal_a_cualquiera(todo_a_decimal(numero_binario),c=8)}")
     # Inicializamos c=16 para que 16 sea el divisor y poder obtener el número hexadecimal
     print(f"{numero_binario} en hexa es: {decimal_a_cualquiera(todo_a_decimal(numero_binario),c=16)}")
+
+if op == 2:
+    
+    numero_decimal = int(input("Ingrese un número decimal: "))
+    # Inicializamos c=2 para que 2 sea el divisor y poder obtener el número binario
+    print(f"{numero_decimal} en binario es: {decimal_a_cualquiera(numero_decimal, c=2)}")
+
+    # Inicializamos c=3 para que 3 sea el divisor y poder obtener el número terciario
+    print(f"{numero_decimal} en terciario es: {decimal_a_cualquiera(numero_decimal, c=3)}")
+
+    # Inicializamos c=4 para que 4 sea el divisor y poder obtener el número cuarternario
+    print(f"{numero_decimal} en cuarternario es: {decimal_a_cualquiera(numero_decimal, c=4)}")
+
+    # Inicializamos c=8 para que 8 sea el divisor y poder obtener el número octal
+    print(f"{numero_decimal} en octal es: {decimal_a_cualquiera(numero_decimal, c=8)}")
+    
+    # Inicializamos c=16 para que 16 sea el divisor y poder obtener el número hexadecimal
+    print(f"{numero_decimal} en hexadecimal es: {decimal_a_cualquiera(numero_decimal, c=16)}")
+
